51a22a50ad72ae47ff88f9ca2ccf8e2d456389ad
Prince Maphupha <princemaphupha@Princes-MacBook-Air.local>
2024-06-20 08:52:57 +0200

    feat: fileread

--- password_cracker.py
+++ password_cracker.py
@@ -1,4 +1,11 @@
 import hashlib
 
+
+
 def crack_sha1_hash(hash, use_salts = False):
+    a = 0
+    with open('top-10000-passwords.txt', 'r') as file:
+        for line in file:
+            if(not(use_salts)):
+               pass
     return True
